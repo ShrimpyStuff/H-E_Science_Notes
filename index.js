1b42fb43dd8f25eca37d71c21117fc258d0649b2
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-19 11:41:02 -0400

    Why won't this work

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 let rawFileData = '';
-function fetchData() {
 fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.txt')
   .then(response => response.text())
   .then((data) => {
     rawFileData = data;
   });
-}
-fetchData();
-
-let jsondata;
-fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.json').then(
-        function(u){ return u.json();}
-      ).then(
-        function(json){
-          jsondata = json;
-        }
-      )
-
-console.log(jsondata);
+console.log(rawFileData);
