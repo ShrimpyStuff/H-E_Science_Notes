61f9d411adcbde2d3d6e306fe2ba225385f1342b
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-19 11:02:11 -0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var client = new XMLHttpRequest();
-client.open('GET', './file.txt');
-client.onreadystatechange = function() {
-  alert(client.responseText);
-}
-client.send();
+fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.txt')
+  .then(response => response.text())
+  .then(data => console.log(data));
