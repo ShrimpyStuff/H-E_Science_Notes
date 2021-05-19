b5e99a237e6f44992c54a7253d5c7a3270192a02
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-19 11:35:37 -0400

    Update

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+let rawFileData = '';
+function fetchData() {
 fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.txt')
   .then(response => response.text())
-  .then(data => console.log(data));
+  .then((data) => {
+    rawFileData = data;
+  });
+}
+fetchData();
+
+let jsondata;
+fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.json').then(
+        function(u){ return u.json();}
+      ).then(
+        function(json){
+          jsondata = json;
+        }
+      )
+
+console.log(jsondata);
