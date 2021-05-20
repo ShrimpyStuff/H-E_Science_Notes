d50bbcb4b392978c8fa5b543b4105d15da1b0cf1
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-20 09:18:10 -0400

    Upload

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+async function doEverything() {
 let rawFileData = '';
-fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.txt')
-  .then(response => response.text())
-  .then((data) => {
-    rawFileData = data;
+  await fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.txt')
+    .then(response => response.text())
+    .then((data) => {
+      rawFileData = data;
+    });
+  
+  let rol = "right";
+
+  rawFileData.split("New Note").forEach(note => {
+    document.getElementById("notes").innerHTML += `<br/><br/><div class="${rol}">${note}</div>`;
+    rol = (rol === right) ? "left" : "right";
   });
-console.log(rawFileData);
+}
+doEverything();
