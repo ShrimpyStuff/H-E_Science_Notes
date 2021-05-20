8840701497b857822aaf27a5ed297f180badbd0b
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-20 13:13:13 -0400

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function doEverything() {
 let rawFileData = '';
-  await fetch('https://shrimpystuff.github.io/H-E_Science_Notes/file.txt')
+  await fetch('/file.txt')
     .then(response => response.text())
     .then((data) => {
       rawFileData = data;
